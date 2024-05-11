4e04471ddcb133e73481240d2b938016c82ee9cd
Ishwar Patel <ishwarrpatel22@gmail.com>
2024-05-11 13:06:38 +0530

    docs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import {FaRegFileAlt} from 'react-icons/fa'
-const Card = () => {
+import { FaDownload } from "react-icons/fa6";
+import { GrFormClose } from "react-icons/gr";
+import { motion } from "framer-motion"
+const Card = ({data, reference}) => {
   return (
-    <>
-       <div className='relative w-52 h-64 rounded-[50px] bg-zinc-900/90 text-white p-5 overflow-hidden'>
+  
+       <motion.div drag dragConstraints={reference} dragTransition={{bounceStiffness: 600, bounceDamping: .1}} whileDrag={{scale: 1.2}} drafElastic={.2} className='relative w-52 h-64 rounded-[50px] bg-zinc-900/90 text-white p-5 overflow-hidden'>
         <FaRegFileAlt/>
-        <p className='text-md mt-5 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing.</p> 
-        <div className='footer absolute bottom-0 bg-sky-200 w-full h-10 left-0'> </div>
+        <p className='text-md mt-5 leading-tight font-semibold'>{data.desc}</p> 
+        <div className='footer absolute bottom-0  w-full left-0'>
+            <div className='flex items-center justify-between py-1 px-8  mb-3'>
+               <h5>
+               {data.fileSize}
+                </h5>
+                <span className='w-5 h-5 bg-sky-200 rounded-full flex items-center justify-center'>
+                  {data.close ? <GrFormClose /> : <FaDownload  size=".8em" color='#fff' /> }
+              
+                </span>
+      </div>
+      {
+        data.tag.isOpen &&  <div className='tag w-full py-4 bg-${data.tag.tagColor}-600 bg-sky-200 flex items-center justify-center'>
+        <h3 className='text-md '>
+        {data.tag.tagTitle}
+          </h3>
+      </div>
+      }
+     
+          
+        </div>
        
-        data
-        tag
+     
+       </motion.div>
 
-       </div>
-    </>
   )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
+
 const Foreground = () => {
+  const ref = useRef(null )
+
+  const data = [
+    {
+      desc: "lorem ipsum dolor sit amet consecdaw dhelilaskds.", 
+      fileSize: ".9mb", 
+      close: true, 
+      tag: {isOpen: true, 
+      tagTitle: "Download Now", 
+      tagColor: "green"}},
+      {
+        desc: "lorem ipsum dolor sit amet consecdaw dhelilaskds.", 
+        fileSize: ".9mb", 
+        close: true, 
+        tag: {isOpen: true, 
+        tagTitle: "Download Now", 
+        tagColor: "green"}}
+      ,{
+        desc: "lorem ipsum dolor sit amet consecdaw dhelilaskds.", 
+        fileSize: ".9mb", 
+        close: true, 
+        tag: {isOpen: true, 
+        tagTitle: "Download Now", 
+        tagColor: "green"}}]
   return (
     <>
-    <div className="fixed top-0 left-0 z-[3] w-full h-full ">
-    <Card/>
+    <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap p-5">
+    {data.map((item, index) =>(
+      <Card data={item} reference={ref}/>
+     ) )}
     </div>
     </>
   )
